e30a1b4f8c435ef655abc96fbb3299ad43eec744
deathworm1 <55323656+deathworm1@users.noreply.github.com>
2022-10-17 20:54:32 +0800

    Add files via upload

---
+++ FloorSquareRootTest.java
@@ -0,0 +1,17 @@
+package test.java.Samples;
+
+import main.java.Samples.FloorSquareRoot;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FloorSquareRootTest {
+    @Test
+    public void testA() {
+        assertEquals(3, FloorSquareRoot.floorSqrt(11));
+    }
+
+    @Test
+    public void testB() {
+        assertEquals(4, FloorSquareRoot.floorSqrt(20));
+    }
+}
